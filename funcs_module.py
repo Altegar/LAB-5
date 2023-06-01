32b8396422977fffbb4b091a1801cbbca83be558
Altegar <91095463+Altegar@users.noreply.github.com>
2023-06-02 00:37:13 +0300

    Some changes

--- funcs_module.py
+++ funcs_module.py
@@ -1,7 +1,7 @@
 from math import *
 
 
-def calculate_expression(expression: str, start: float, end: float, step: int) -> list:
+def calculate_expression(expression: str, start: int, end: int, step: int) -> list:
     """
     Обчислює переданий вираз з невідомою змінною x за допомогою табулювання функції на певному проміжку.
 
@@ -9,9 +9,9 @@ def calculate_expression(expression: str, start: float, end: float, step: int) -
     -----------
         expression (str): вираз
 
-        start (float): початок проміжку табулювання
+        start (int): початок проміжку табулювання
 
-        end (float): кінець проміжку табулювання
+        end (int): кінець проміжку табулювання
 
         step (int): крок табулювання
 
@@ -28,11 +28,9 @@ def calculate_expression(expression: str, start: float, end: float, step: int) -
     [3, 7, 11, 15, 19, 23]
     """
     values = []  # Список значень змінної x
-    x = start  # Присвоєння змінній x початкового значення
-    while x <= end:
+    for x in range(start, end + 1, step):
         result = eval(expression)  # Обчислюємо вираз
         values.append(round(result, 3))  # Додаємо результат до списку та округлюємо результат до 3 знаків після коми
-        x += step
 
     return values
 
--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Лабораторна робота №5
 # Функції
 
-import funcs_module as fm
+from funcs_module import *
 
 a = int(input("Введіть початок проміжку: a = "))
 b = int(input("Введіть кінець проміжку: b = "))
@@ -11,9 +11,8 @@ h = int(input("Введіть крок проміжку: h = "))
 y1 = "2 * (x ** 2 - 3) - (sin(x) ** 2 / (1 + cos(x))) + cos(abs(x)) ** 2 - log(abs(x) + 2) - 1 + sin(x) ** 2"
 y2 = "7 - 4 * x - x ** 2 + x ** 3 + log(3 + 2 * abs(x)) + 1 / tan((4 + abs(x)) / 8) + sqrt(1 - sin(x) ** 2)"
 
-result1 = fm.calculate_expression(y1, a, b, h)
-result2 = fm.calculate_expression(y2, a, b, h)
+result1 = calculate_expression(y1, a, b, h)
+result2 = calculate_expression(y2, a, b, h)
 
-print('\n'
-      f"Варіант 18: {result1}\n"
-      f"Варіант 38: {result2}")
+print(f"Варіант 18: {result1}")
+print(f"Варіант 38: {result2}")
